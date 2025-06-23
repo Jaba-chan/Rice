864a8654f94af5d7d6a34b27e346202bb6f2e574
kuzak <kuzakovevgen@gmail.com>
2025-06-24 00:24:55 +0700

    Creating theme, colors, typography

---
+++ core/designsystem/src/commonMain/kotlin/ru/evgenykuzakov/designsystem/Color.kt
@@ -0,0 +1,12 @@
+package ru.evgenykuzakov.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val WeakWhite = Color(0xFF999FA5)
+val Blue = Color(0xFF8AB4F8)
+val StrongGray = Color(0xFF1E1F22)
+val DarkGray = Color(0xFF2B2D30)
+val Gray = Color(0xFF414141)
+val LightGray = Color(0xFF545454)
